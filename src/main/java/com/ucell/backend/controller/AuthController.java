d7b8612df36d9c4fc6bff183cb3fa352d170334b
khojiyev.edu@gmail.com <khojiyev.edu@gmail.com>
2024-02-24 12:53:58 +0500

    auth controller reformat

--- src/main/java/com/ucell/backend/controller/AuthController.java
+++ src/main/java/com/ucell/backend/controller/AuthController.java
@@ -4,9 +4,8 @@ package com.ucell.backend.controller;
 import com.ucell.backend.config.JwtUtil;
 import com.ucell.backend.entity.Users;
 import com.ucell.backend.response.ApiResponseToken;
-import com.ucell.backend.response.ApiResponseV1;
+import com.ucell.backend.service.UsersService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,37 +16,19 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
+    private final UsersService usersService;
 
-
-    private final JwtUtil jwtUtil;
-    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-
+    public AuthController(UsersService usersService) {
+        this.usersService = usersService;
     }
 
     @ResponseBody
     @RequestMapping(value = "/get-token",method = RequestMethod.POST)
     public ApiResponseToken login(@RequestParam("login") String userName,
-                                @RequestParam("secret") String password)  {
+                                @RequestParam("password") String password)  {
 
-        try {
-            Authentication authentication =
-                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, password));
-            String name = authentication.getName();
-            Users user = new Users(name,password);
-            String token = jwtUtil.createToken(user);
+        return usersService.getToken(userName,password);
 
-            return new ApiResponseToken(name,token,"",HttpStatus.OK);
-
-
-        }catch (BadCredentialsException e){
-            return new ApiResponseToken("Invalid username or password",HttpStatus.BAD_REQUEST);
-        }catch (Exception e){
-            return new ApiResponseToken("credential error",HttpStatus.BAD_REQUEST);
-
-        }
     }
 }
 
--- src/main/java/com/ucell/backend/service/UsersService.java
+++ src/main/java/com/ucell/backend/service/UsersService.java
@@ -1,11 +1,17 @@
 package com.ucell.backend.service;
 
+import com.ucell.backend.config.JwtUtil;
 import com.ucell.backend.entity.Users;
 import com.ucell.backend.repository.UserRepository;
+import com.ucell.backend.response.ApiResponseToken;
 import com.ucell.backend.response.UserSearchingResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -15,8 +21,42 @@ import java.util.Optional;
 @Log4j2
 public class UsersService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+
+
+    private final JwtUtil jwtUtil;
+
+    private final UserRepository userRepository;
+
+    public UsersService(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+
+    public ApiResponseToken getToken(String userName,String password){
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, password));
+            String name = authentication.getName();
+            Users user = new Users(name,password);
+            String token = jwtUtil.createToken(user);
+
+            if (name.isBlank()){
+                return new ApiResponseToken("Invalid username or password",HttpStatus.BAD_REQUEST);
+            }
+
+            return new ApiResponseToken(name,token,"",HttpStatus.OK);
+
+
+        }catch (BadCredentialsException e){
+            return new ApiResponseToken("Invalid username or password",HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ApiResponseToken("credential error",HttpStatus.BAD_REQUEST);
+
+        }
+    }
 
     public UserSearchingResponse isRightUserWithPass(String user, String password) throws Exception {
 
